49a193b6df3f64b90f6ae1533ebad52261640e33
Lu Xitian <1198648104@qq.com>
2024-12-15 22:46:56 -0800

    kyx

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -7,6 +7,7 @@ int main()
 {JINNIE
     std::cout << "Hello World!\n";
     std::cout << "kyx!\n";
+    std::cout << "lxt!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
